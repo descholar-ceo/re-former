ec3e32cc5709b09cb84faab79f3a30332102c8d4
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-18 16:38:30 +0200

    fix rubocop lint error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
   def new
     @user = User.new
   end
@@ -15,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
