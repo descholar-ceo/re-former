7b94b64fbc9bfcbc5ab356c18150066727205407
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-18 14:11:23 +0200

    Fix rubocop lint errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
-    include UsersHelper
-    def new
-        @user = User.new()
+  include UsersHelper
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to new_user_path
-        else
-            render :new
-        end
-    end
+  end
 end
