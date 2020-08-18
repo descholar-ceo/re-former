577be59a7e784b773367446e078746692af08485
descholar-ceo <emmanuellamugi@gmail.com>
2020-08-18 13:07:46 +0200

    Add actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+    include UsersHelper
     def new
     end
     def create
+        @user = User.new(user_params)
+        @user.save
+        redirect_to new_user_path
     end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+    def user_params
+        puts "The params are : #{params}"
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
